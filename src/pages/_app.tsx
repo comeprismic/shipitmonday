328b944c69f5ecb86577aa448bca9d41cdcd3a62
Come Sabran <comesabran@comes-macbook-pro.home>
2023-09-26 12:34:51 +0200

    edited local fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,12 +7,36 @@ import clsx from 'clsx';
 import '../../public/reset.css'; // Import the reset CSS file
 
 const RationalDisplay = localFont({ src: './RationalDisplay-Bold.otf', variable: "--font-rationaldisplay" })
-const Satoshi = localFont({ src: './Satoshi-Bold.otf', variable: "--font-satoshi" })
 const NanumPen = localFont({ src: './NanumPenScript-Regular.ttf', variable: "--font-nanumpen" })
+const Satoshi = localFont({
+  src: [
+    {
+      path: './Satoshi/Satoshi-Bold.otf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: './Satoshi/Satoshi-BoldItalic.otf',
+      weight: '900',
+      style: 'italic',
+    },
+    {
+      path: './Satoshi/Satoshi-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './Satoshi/Satoshi-MediumItalic.otf',
+      weight: '500',
+      style: 'italic',
+    },
+  ],
+  variable: "--font-satoshi"
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={clsx(RationalDisplay.variable, Satoshi.variable, NanumPen.variable, Satoshi.className)}>
+    <div className={clsx(RationalDisplay.variable, NanumPen.variable, Satoshi.variable, Satoshi.className)}>
       <Component {...pageProps} />
       <PrismicPreview repositoryName={repositoryName} />
     </div>
